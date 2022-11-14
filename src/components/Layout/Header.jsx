26d64d4a58484cbd2d2c7e274c8b53d232f72131
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-11-14 19:00:11 +0300

    Header is implemented.

---
+++ src/components/Layout/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="circle_container">
+        <div className="circle red" />
+        <div className="circle yellow" />
+        <div className="circle green" />
+      </div>
+        <div className="header_text">batuhan_ulucay_portfolio</div>
+    </div>
+  );
+}
+
+export default Header;
