1d497a84ab2b83e83c02a2bfd5556c3f1747dde1
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-11-14 23:35:13 +0300

    Explorer is added.

--- src/components/Layout/Explorer.jsx
+++ src/components/Layout/Explorer.jsx
@@ -3,8 +3,14 @@ import React from "react";
 function Explorer() {
   return (
     <div className="explorer">
-      <p style={{color: "white", marginLeft: "1.5rem"}}>EXPLORER</p>
-      <h3 style={{color: "white", marginLeft: "1.5rem"}}>Portfolio</h3>
+      <p style={{ color: "white", marginLeft: "1.5rem" }}>EXPLORER</p>
+      <h4 style={{ color: "white", marginLeft: "1.5rem" }}>PORTFOLIO</h4>
+      <div className="files">
+        <div className="file">home.jsx</div>
+        <div className="file">projects.jsx</div>
+        <div className="file">resume.jsx</div>
+        <div className="file">contact.jsx</div>
+      </div>
     </div>
   );
 }
