a7fc574f1b558c2a2aeeedba48987ce6b1af5025
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-11-16 17:20:58 +0300

    Home tabs content is added.

--- src/components/Contents/Home.jsx
+++ src/components/Contents/Home.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import photo from "../../assets/photo.jpeg";
 
 function Home() {
   return (
-    <div className='home'>Home</div>
-  )
+    <>
+      <div className="home">
+        <div className="img-container">
+          <img src={photo} alt="me" className="photo" />
+        </div>
+        <div className="about-me">
+          <p style={{ color: "#68C9F8" }}>console</p>.
+          <p style={{ color: "#D6B879" }}>log</p>
+          <p style={{ color: "#D4678F" }}>(</p>
+          <p style={{ color: "#C96D3D" }}>
+            "Hello, I am Batuhan. I am a Fullstack Developer.
+            <br /> I worked on self-driving cars for 3 years and recently I
+            decided to become a web developer."
+          </p>
+          <p style={{ color: "#D4678F" }}>)</p>;
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
