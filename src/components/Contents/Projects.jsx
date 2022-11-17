38df87f255991ce7fbaf61b6bc26a83c5aa820df
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-11-17 15:24:06 +0300

    project details are added.

--- src/components/Contents/Projects.jsx
+++ src/components/Contents/Projects.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import { projects } from "../../projects";
 
 function Projects() {
   return (
-    <div>Projects</div>
-  )
+    <div className="projects">
+      <div className="projects-grid">
+        {projects.map((project) => (
+          <div className="project-card">
+            <img src={project.src} alt="project" />
+            <h2>{project.name}</h2>
+            <p>{project.description}</p>
+            {/* gh link here */}
+            {/* live demo link here */}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Projects
+export default Projects;
---
+++ src/projects.js
@@ -0,0 +1,34 @@
+import f1 from "./assets/projects/f1.png";
+import poke from "./assets/projects/poke.png"
+import todo from "./assets/projects/todo.png"
+
+export const projects = [
+  {
+    name: "F1-react",
+    src: f1,
+    description: "An informative unofficial Formula 1 page for fans.",
+    gh_link: "https://github.com/BatuhanUlucay/F1-react",
+    live_link: "https://f1-react.vercel.app/",
+  },
+  {
+    name: "react-pokedex",
+    src: poke,
+    description: "Simple ui for displaying pokemons.",
+    gh_link: "https://github.com/BatuhanUlucay/react-pokedex",
+    live_link: "https://react-pokedex-alpha.vercel.app/",
+  },
+  {
+    name: "Github Finder",
+    src: f1,
+    description: "Simple ui for searching github users, viewing their repos and some other details.",
+    gh_link: "https://github.com/BatuhanUlucay/github-finder",
+    live_link: "",
+  },
+  {
+    name: "popupsmart-todo",
+    src: todo,
+    description: "A to do list with basic CRUD operations with a mock api.",
+    gh_link: "https://github.com/BatuhanUlucay/popupsmart_todo",
+    live_link: "https://popupsmart-todo.vercel.app/",
+  },
+];
