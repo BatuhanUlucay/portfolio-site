8a2bc9b36e360d3a85d78caf8b0a59850abbe7d2
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-11-18 16:01:39 +0300

    contact layout is briefly created

--- src/components/Contents/Contact.jsx
+++ src/components/Contents/Contact.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { AiFillLinkedin } from "react-icons/ai";
+import { AiFillGithub } from "react-icons/ai";
 
 function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="contact">
+
+      <h2>Feel free to contact me!</h2>
+
+      <div className="linkedin">
+        <h2 style={{ color: "#D7BA7D" }}>{".linkedin {"}</h2>
+        <AiFillLinkedin
+          size={"40px"}
+          color="#FFFFFF"
+          // style={{ marginLeft: "2rem" }}
+        />
+        <h2 style={{ color: "#D7BA7D" }}>{"}"}</h2>
+      </div>
+
+      <div className="github">
+        <h2 style={{ color: "#D7BA7D" }}>{".github {"}</h2>
+        <AiFillGithub
+          size={"40px"}
+          color="#FFFFFF"
+          // style={{ marginLeft: "2rem" }}
+        />
+        <h2 style={{ color: "#D7BA7D" }}>{"}"}</h2>
+      </div>
+      <div className="calendly"></div>
+    </div>
+  );
 }
 
-export default Contact
+export default Contact;
