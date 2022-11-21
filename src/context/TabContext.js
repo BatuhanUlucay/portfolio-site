433ca0593ff2632b69bd3d6752f71b42391b663b
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-11-21 23:07:46 +0300

    cursor style on files is updated

--- src/context/TabContext.js
+++ src/context/TabContext.js
@@ -6,7 +6,7 @@ import { FaCss3 } from "react-icons/fa";
 
 export const tabs = [
   { name: "home.js", logo: <SiJavascript color="yellow" className="logo" /> },
-  { name: "projects.json", logo: <VscJson color="yellow" className="logo" /> },
+  { name: "projects.json", logo: <VscJson color="yellow" size={"1rem"} className="logo" /> },
   {
     name: "resume.pdf",
     logo: <BsFillFileEarmarkPdfFill color="red" className="logo" />,
