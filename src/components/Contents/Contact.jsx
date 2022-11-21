b54058adc1b5b370616c61c9f16472a77728990b
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-11-21 17:05:02 +0300

    Styling

--- src/components/Contents/Contact.jsx
+++ src/components/Contents/Contact.jsx
@@ -6,28 +6,33 @@ import { InlineWidget } from "react-calendly";
 function Contact() {
   return (
     <div className="contact">
-
-      <h2>Feel free to contact me!</h2>
+      <h1 style={{ color: "white" }}>Feel free to contact me!</h1>
 
       <div className="linkedin">
-        <h2 style={{ color: "#D7BA7D" }}>{".linkedin {"}</h2>
-        <AiFillLinkedin
-          size={"40px"}
-          color="#FFFFFF"
-        />
+        <h2 style={{ color: "#D7BA7D", marginTop: "3rem" }}>{".linkedin {"}</h2>
+        <a
+          href="https://www.linkedin.com/in/batuhan-ulu%C3%A7ay-0aa6b9125/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <AiFillLinkedin size={"40px"} color="#FFFFFF" />
+        </a>
         <h2 style={{ color: "#D7BA7D" }}>{"}"}</h2>
       </div>
 
       <div className="github">
         <h2 style={{ color: "#D7BA7D" }}>{".github {"}</h2>
-        <AiFillGithub
-          size={"40px"}
-          color="#FFFFFF"
-        />
+        <a
+          href="https://github.com/BatuhanUlucay"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <AiFillGithub size={"40px"} color="#FFFFFF" />
+        </a>
         <h2 style={{ color: "#D7BA7D" }}>{"}"}</h2>
       </div>
       <div className="calendly">
-      <InlineWidget url="https://calendly.com/batuhanulucay/30min" />
+        <InlineWidget url="https://calendly.com/batuhanulucay/30min" />
       </div>
     </div>
   );
