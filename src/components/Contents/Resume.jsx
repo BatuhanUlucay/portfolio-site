c16335f013fdfc8b26533d11b2c5be6675af9c82
BatuhanUlucay <batuhanulucay00@gmail.com>
2022-11-21 18:00:30 +0300

    cv is added to resume page.

--- src/components/Contents/Resume.jsx
+++ src/components/Contents/Resume.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Document, Page } from "react-pdf/dist/esm/entry.webpack5";
+import cv from "../../assets/cv.pdf";
 
 function Resume() {
+
   return (
-    <div>Resume</div>
-  )
+    <div className="resume">
+      <Document file={cv} >
+        <Page pageNumber={1} scale="1.5" className="resume-page"/>
+      </Document>
+    </div>
+  );
 }
 
-export default Resume
+export default Resume;
